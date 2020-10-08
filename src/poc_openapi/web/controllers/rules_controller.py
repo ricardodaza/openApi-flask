32e708796a0d2c24d9322d8fa24db7c66a5df049
Ricardo Daza <ricardo.daza@amplia.es>
2020-10-08 13:23:25 +0200

    CRUD

--- src/poc_openapi/web/controllers/rules_controller.py
+++ src/poc_openapi/web/controllers/rules_controller.py
@@ -2,11 +2,10 @@ import connexion
 import six
 
 from poc_openapi.web.models.rule import Rule  # noqa: E501
-from poc_openapi.web.models.rules import Rules
+from poc_openapi.web.models.rules import Rules  # noqa: E501
 from poc_openapi.web import util
 
 from poc_openapi.core import rules
-
 
 def add_rule(body):  # noqa: E501
     """Add a new rule to opengate
@@ -24,51 +23,67 @@ def add_rule(body):  # noqa: E501
     return {}, 201
 
 
-def delete_rule(ruleId, api_key=None):  # noqa: E501
+def delete_rule(rule_id):  # noqa: E501
     """Deletes a rule
 
      # noqa: E501
 
-    :param ruleId: Rule id to delete
-    :type ruleId: str
-    :param api_key: 
-    :type api_key: str
+    :param rule_id: Rule id to delete
+    :type rule_id: str
 
-    :rtype: None 
+    :rtype: None
     """
-    return 'do some magic!'
+    try:
+        rules.remove_rule(rule_id)
+        response = {}, 200
+    except KeyError:
+        response = {}, 404
+
+    return response
 
 
-def get_rule_by_id(ruleId):  # noqa: E501
+def get_all_rules():  # noqa: E501
+    """Gets all rules
+
+     # noqa: E501
+
+
+    :rtype: Rules
+    """
+    allRules = rules.get_all_rules()
+    return allRules, 200
+
+
+def get_rule_by_id(rule_id):  # noqa: E501
     """Find rule by ID
 
-    Returns a single rule # noqa: E501
+     # noqa: E501
 
-    :param ruleId: ID of rule to return
-    :type ruleId: str
+    :param rule_id: ID of rule to return
+    :type rule_id: str
 
     :rtype: Rule
     """
     try:
-        rule = rules.get_rule(ruleId)
+        rule = rules.get_rule(rule_id)
         response = rule, 200
     except KeyError:
         response = {}, 404
     return response
 
 
-def update_rule(ruleId, name=None, javascript=None):  # noqa: E501
+def update_rule(rule_id, Rule):  # noqa: E501
     """Updates a rule in the opengate platform
 
      # noqa: E501
 
-    :param ruleId: ID of rule that needs to be updated
-    :type ruleId: str
-    :param name: Updated name of the rule
-    :type name: str
-    :param javascript: Updated javascript of the rule
-    :type javascript: str
+    :param rule_id: ID of rule that needs to be updated
+    :type rule_id: str
+    :param Rule: 
+    :type Rule: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        Rule = Rule.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
